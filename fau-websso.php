d7ecef994d41d85d30af45865c59b8abd55be586
rvdforst <rolf.v.d.forst@fau.de>
2020-06-10 12:57:28 +0200

    Update Autoloader (PSR-4)

--- fau-websso.php
+++ fau-websso.php
@@ -23,6 +23,7 @@ defined('ABSPATH') || exit;
 const RRZE_PHP_VERSION = '7.4';
 const RRZE_WP_VERSION = '5.4';
 
+// Autoloader (PSR-4)
 spl_autoload_register(function ($class) {
     $prefix = __NAMESPACE__;
     $base_dir = __DIR__ . '/includes/';
@@ -32,8 +33,8 @@ spl_autoload_register(function ($class) {
         return;
     }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $relativeClass = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relativeClass) . '.php';
 
     if (file_exists($file)) {
         require $file;
